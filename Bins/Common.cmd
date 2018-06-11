607155ce864c09fe23d9c6b03a7d29b461ac97cc
Martin Pavlicek <pavlim45@fit.cvut.cz>
2018-06-11 13:02:57 +0200

    Added path notice

--- Bins/Common.cmd
+++ Bins/Common.cmd
@@ -2,3 +2,11 @@ echo Deep recommender Image-KNN by Martin Pavlicek 2018
 
 rem Use following line if you use custom cmd environment
 rem call activate deeplearning
+
+rem Remove folowing lines when you are done with setup procedure
+echo .
+echo Edit batch paths to fit your configuration first!
+echo When you are done, remove this message.
+echo .
+pause
+exit
